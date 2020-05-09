da623891e58b732a897897859904297f791b4af6
df-HelloWorld <duanfeng_1712@qq.com>
2020-05-09 21:02:03 +0800

    new project

---
+++ hz-source-master/src/main/java/com/hz/source/master/HzSourceApplication.java
@@ -0,0 +1,13 @@
+package com.hz.source.master;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HzSourceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HzSourceApplication.class, args);
+    }
+
+}
