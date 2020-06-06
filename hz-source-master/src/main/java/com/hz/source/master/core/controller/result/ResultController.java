33220971802b9e09fe4957845b26f5ff92dcd783
tangzhulong12 <345061813@qq.com>
2020-06-06 15:44:10 +0800

    添加银行卡、微信回执

--- hz-source-master/src/main/java/com/hz/source/master/core/controller/result/ResultController.java
+++ hz-source-master/src/main/java/com/hz/source/master/core/controller/result/ResultController.java
@@ -1,7 +1,9 @@
 package com.hz.source.master.core.controller.result;
 
 import com.hz.source.master.core.common.utils.JsonResult;
+import com.hz.source.master.core.common.utils.StringUtil;
 import com.hz.source.master.core.controller.question.QuestionController;
+import com.hz.source.master.core.model.ResponseEncryptionJson;
 import com.hz.source.master.core.model.result.CatAllDataModel;
 import com.hz.source.master.core.model.result.MobileCardDataModel;
 import com.hz.source.master.core.model.sms.SmsData;
@@ -24,18 +26,23 @@ import java.util.Map;
 @RestController
 @RequestMapping("/source/rs")
 public class ResultController {
-    private static Logger log = LoggerFactory.getLogger(QuestionController.class);
+    private static Logger log = LoggerFactory.getLogger(ResultController.class);
 
 
 //    http://192.168.1.156:8087/source/rs/sms_rs
 
     @RequestMapping(value = "/sms", method = {RequestMethod.POST})
     public JsonResult<Object> smsRs(HttpServletRequest request, HttpServletResponse response, @RequestBody SmsData smsData) throws Exception{
+        String sgid = ComponentUtil.redisIdService.getNewId();
+        String cgid = "";
+        String token;
+        String ip = StringUtil.getIpAddress(request);
+
+        String data = "";
         log.error("SecretKey:"+smsData.getSecretKey());
         log.error("sender:"+smsData.getSender());
         log.error("PhoneId:"+smsData.getPhoneId());
         log.error("Content:"+smsData.getContent());
-
         boolean  flag  =   WecharMethod.isEffectiveSmsData(smsData);//是否有效
 
         if(!flag){
@@ -46,24 +53,30 @@ public class ResultController {
 
         try{
             ComponentUtil.mobileCardDataService.addMobileCardData(mobileCardDataModel);
+            ResponseEncryptionJson resultDataModel = new ResponseEncryptionJson();
+            resultDataModel.jsonData = "ok";
+            // 返回数据给客户端
+            return JsonResult.successResult(resultDataModel, cgid, sgid);
         }catch (Exception e){
             e.printStackTrace();
+            return JsonResult.failedResult("数据异常","500");
         }
 
 
 //        mobileCardDataService
 
-        return JsonResult.successResult("", "", "");
     }
 
     @PostMapping(value = "/wechat")
     public JsonResult<Object> wechat(HttpServletRequest request, HttpServletResponse response, @RequestParam Map<String, Object> obj) throws Exception{
         //判断是需要的状态码
 //        ComponentUtil.dispatchService.disp(lovelyCatData);
+        log.info("==============:进来了！");
+        System.out.println("==================="+obj.get("msg_type"));
         CatAllDataModel catAllDataModel=WecharMethod.toCatAllDataModel(obj);
         ComponentUtil.catAllDataModelService.addCatAllDataModel(catAllDataModel);
 
-        //内容判断 1、取消管理员 2、添加管理员 3、支付内容
+        //内容判断 1、取消管理员 2、添加 管理员 3、支付内容
         return JsonResult.successResult("", "", "");
     }
 }
