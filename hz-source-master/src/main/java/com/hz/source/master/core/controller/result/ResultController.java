89460f993a2195632b22af34d5005a01d3e8efab
tangzhulong12 <345061813@qq.com>
2020-08-17 15:18:02 +0800

    添加新的微信监控

--- hz-source-master/src/main/java/com/hz/source/master/core/controller/result/ResultController.java
+++ hz-source-master/src/main/java/com/hz/source/master/core/controller/result/ResultController.java
@@ -118,7 +118,7 @@ public class ResultController {
             String dataStr = "";
             if (obj != null && obj.size() > 0){
                 dataStr = obj.get("data").toString();
-                dataStr = StringUtil.decoderBase64(dataStr);
+                dataStr = StringUtil.decoderBase64ByUTF8(dataStr);
                 dataStr = dataStr.replaceAll("\"", "~");
                 dataStr = dataStr.replaceAll("'", "\"");
                 WxAllDataModel wxAllDataModel= WecharMethod.toWxAllDataModel(dataStr);
